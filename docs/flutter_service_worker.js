0a2550a202359aa0b9ac2630ed00a80f3c653f6d
JianWang0128 <jian645558352@163.com>
2026-01-02 16:28:15 +0800

    更新Web构建文件

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "040ded1a0f5f1ba1cffef219890c7f33",
+const RESOURCES = {"flutter_bootstrap.js": "fe8eac6128ee48cbe988b682e6efbb69",
 "version.json": "8a2522023156e367e7da39a582030613",
 "index.html": "f900a5e0b817b24fa643d70ab9d3cf6c",
 "/": "f900a5e0b817b24fa643d70ab9d3cf6c",
